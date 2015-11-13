df9177169ae3f656ea97c250e093dfcdf44adddb
Galliez Bruno <galliez.bruno@gmail.com>
2015-11-13 10:44:05 +0100

    Start lab05

---
+++ Galliez-Bruno-Lab05/Galliez-Bruno-Lab05/Source.cpp
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+void dessine() {
+
+	// Vide la console
+	system("cls");
+
+	printf("*********************************************************************************************************************************\n");
+	printf("**                                                                                                                             **\n");
+	printf("**                                    _                      _______                      _                                    **\n");
+	printf("**                                 _dMMMb._              .adOOOOOOOOOba.              _,dMMMb_                                 **\n");
+	printf("**                                dP'  ~YMMb            dOOOOOOOOOOOOOOOb            aMMP~  `Yb                                **\n");
+	printf("**                                V      ~\"Mb          dOOOOOOOOOOOOOOOOOb          dM\"~      V                                **\n");
+	printf("**                                         `Mb.       dOOOOOOOOOOOOOOOOOOOb       ,dM'                                         **\n");
+	printf("**                                          `YMb._   |OOOOOOOOOOOOOOOOOOOOO|   _,dMP'                                          **\n");
+	printf("**                                            `YMMM| OP'~~YOOOOOOOOOOOP ~~`YO |MMMP'                                           **\n");
+	printf("**                                   ,dMMMb.     ~~' OO     `YOOOOOP'     OO `~~     ,dMMMb.                                   **\n");
+	printf("**                                _,dP~  `YMba_      OOb      `OOO'      dOO      _aMMP'  ~Yb._                                **\n");
+	printf("**                               <MMP'     `~YMMa_   YOOo   @  OOO  @   oOOP   _adMP~'      `YMM>                              **\n");
+	printf("**                                            `YMMMM\`OOOo     OOO     oOOO'/MMMMP'                                             **\n");
+	printf("**                                    ,aa.     `~YMMb `OOOb._,dOOOb._,dOOO'dMMP~'       ,aa.                                   **\n");
+	printf("**                                  ,dMYYMba._         `OOOOOOOOOOOOOOOOO'          _,adMYYMb.                                 **\n");
+	printf("**                                 ,MP'   `YMMba._      OOOOOOOOOOOOOOOOO       _,adMMP'   `YM.                                **\n");
+	printf("**                                 MP'        ~YMMMba._ YOOOOPVVVVVYOOOOP  _,adMMMMP~       `YM                                **\n");
+	printf("**                                 YMb           ~YMMMM\\`OOOOI`````IOOOOO'/MMMMP~           dMP                                **\n");
+	printf("**                                  `Mb.           `YMMMb`OOOI,,,,,IOOOO'dMMMP'           ,dM'                                 **\n");
+	printf("**                                    `'                  `OObNNNNNdOO'                   `'                                   **\n");
+	printf("**                                                          `~OOOOO~'                                                          **\n");
+	printf("**                                                                                                                             **\n");
+	printf("**                     #####  #####  #####  #####  #    #            #    #  #####  #####  #      ####                         **\n");
+	printf("**                     #      #   #  #   #     #    #  #             #    #  #   #  #   #  #      #   #                        **\n");
+	printf("**                     #      #####  #####    #      ##              # ## #  #   #  #####  #      #   #                        **\n");
+	printf("**                     #      # #    #   #   #       #               ##  ##  #   #  # #    #      #   #                        **\n");
+	printf("**                     #####  #  #   #   #  #####   #                #    #  #####  #  #   #####  ####                         **\n");
+	printf("**                                                                                                                             **\n");
+	printf("**                                                    The badass power !                                                       **\n");
+	printf("*********************************************************************************************************************************\n");
+}
+
+void main() {
+
+	// initialisation varibables
+
+
+
+}
