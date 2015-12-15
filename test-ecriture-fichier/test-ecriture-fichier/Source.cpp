15c72f6d8430cbeee5385f38f915484dd1f74ce7
Galliez Bruno <galliez.bruno@gmail.com>
2015-12-15 07:49:47 +0100

    test cocos2dx

--- test-ecriture-fichier/test-ecriture-fichier/Source.cpp
+++ test-ecriture-fichier/test-ecriture-fichier/Source.cpp
@@ -1,15 +1,24 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void main() {
+void purge(void)
+{
+	int c;
+	while ((c = fgetc(stdin)) != '\n' && c != EOF) {
+	}
+}
 
+
+void main() {
+	/*
 	FILE * monFichier = NULL;
 	monFichier = fopen("test.txt", "r");
 
 	printf("%s", monFichier);
 	system("pause");
+	*/
 
-
-
-
+	int i, j;
+	int matrice[5][5] = {{1,2,3,4,5},{ 1,2,3,4,5 },{ 1,2,3,4,5 },{ 1,2,3,4,5 },{ 1,2,3,4,5 }}
+	system("pause");
 }
